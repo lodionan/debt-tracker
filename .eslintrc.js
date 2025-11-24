426adad8b9eced84564733cd5f2edb7de8f0bf79
lodionan <pablo.moran@outlook.es>
2025-11-24 04:26:10 -0600

    Fix Vercel build issues

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
